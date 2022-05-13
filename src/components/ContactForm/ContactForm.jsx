802cf8de4127c197567852a26154e35321dc3836
Iryna S <irynaradkovska@gmail.com>
2022-05-13 18:03:24 +0300

    adds files from the 2nd hw

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,68 @@
+import { Component } from 'react';
+import s from './ContactForm.module.css';
+import PropTypes from 'prop-types';
+
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: ''
+  };
+    
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    const { name, number } = this.state;
+
+    return (
+        <form onSubmit={this.handleSubmit} className={s.form}>
+          <label className={s.input}>
+            Name
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.handleChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <label className={s.input}>
+            Number
+            <input
+              type="tel"
+              name="number"
+              value={number}
+              onChange={this.handleChange}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+          <button type="submit" className={s.button}>
+            Add contact
+          </button>
+        </form>
+    );
+  }
+};
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired
+}
+
+export default ContactForm;
